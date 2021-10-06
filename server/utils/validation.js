5fecde4f67a25ea3ab1f6f2d5d3e0424d99f11ac
Saurabh <ambardekar.s@northeastern.edu>
2021-10-06 01:23:47 -0400

    Updated validations

---
+++ server/utils/validation.js
@@ -0,0 +1,41 @@
+const validate = (input,data) => {
+    switch (input) {
+        case "userName":
+            var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return re.test(String(data).toLowerCase());
+        case "firstName":
+            var re = /^[A-Za-z]+$/
+            return re.test(data)
+        case "lastName":
+            var re = /^[A-Za-z]+$/
+            return re.test(data)
+        case "password":
+            var re = /^[A-Za-z]+$/
+            return re.test(data)
+    }
+}
+//field validator
+const compare = (key) => {
+    const compare = ["firstName","lastName","password"]
+    let count = 0
+    for (k in key){
+        if (compare.includes(k)){
+            count += 1
+        }
+    }   
+    return count == Object.keys(key).length
+}
+
+const compare2 = (key) => {
+    const compare = ["firstName","lastName","password","userName"]
+    let count = 0
+    for (k in key){
+        if (compare.includes(k)){
+            count += 1
+        }
+    }   
+    return count == Object.keys(key).length
+}
+module.exports = {
+    validate,compare, compare2
+}
