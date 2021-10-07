4328c4df3682413b4bf413c31d379d3cbaea7bdf
Saurabh <ambardekar.s@northeastern.edu>
2021-10-06 20:27:21 -0400

    final test case

--- test/usertest.js
+++ test/usertest.js
@@ -28,21 +28,3 @@ describe('User data should be added if valid',()=>{
         chai.expect(UserId).to.equal(null)
     })
 })
-describe('User data should be added if valid',()=>{
-    let userName = "test@test.com";
-    let password = "test";
-    let firstName = "Test";
-    let lastName = "Example";
-    let Account_Updated = null;
-    let Account_Created = null;
-    let UserId = null;
-    it("Create new user",()=>{
-        chai.expect(userName).to.equal("test@test.com")
-        chai.expect(password).to.equal("test") 
-        chai.expect(firstName).to.equal("Test")
-        chai.expect(lastName).to.equal("Example") 
-        chai.expect(Account_Updated).to.equal(null) 
-        chai.expect(Account_Created).to.equal(null)
-        chai.expect(UserId).to.equal(null)
-    })
-})
