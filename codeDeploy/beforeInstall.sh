b7e2480c355c1c8c3e62f2c4c6b14e5f355747bc
Saurabh <ambardekar.s@northeastern.edu>
2021-11-09 13:00:57 -0500

    pull req 13

--- codeDeploy/beforeInstall.sh
+++ codeDeploy/beforeInstall.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu
 sudo rm -rf webapp
-# echo "get the proces id"
-# PID=`ps -eaf | grep "pm2 start server/app.js" | grep -v grep | awk '{print $2}'`
-# echo "process id not empty ? $PID"
-# if [[ "" !=  "$PID" ]]; then
-#  sudo kill -9 $PID
-# fi
-# sudo fuser -n tcp -k 4000 
-
 PID = `ps aux | grep PM2`
 if [[ "" !=  "$PID" ]]; then
  sudo kill -9 $PID
