64ee19b90b71ff4a6ea6a384f0c648b0b375326a
Saurabh <ambardekar.s@northeastern.edu>
2021-11-14 12:34:49 -0500

    cloud watch agent

--- codeDeploy/afterInstall.sh
+++ codeDeploy/afterInstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo unzip build_artifact.zip
 sudo unzip deploySrcCodeBuild.zip
+aws configure set default.region us-east-1
+aws configure list
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/logs/log.json -s
 cd ..
 cd /home/ubuntu/webapp 
 sudo rm -rf node_modules
--- codeDeploy/applicationStart.sh
+++ codeDeploy/applicationStart.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 cd /home/ubuntu/webapp
-
+if [ -d "logs" ] 
+then
+    echo "Directory /home/centos/webapp/logs exists." 
+else
+    sudo touch logs/webapp.log
+    sudo chmod 666 logs/webapp.log
+fi
 sudo npm install
 sudo npm i pm2 -g
 pm2 start server/app.js >> touch debug.log 2>&1 &
