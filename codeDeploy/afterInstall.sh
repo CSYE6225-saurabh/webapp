d5fb6596809f0c17425bfde767ac5a5c40d3caa0
Saurabh <ambardekar.s@northeastern.edu>
2021-11-14 18:49:08 -0500

    logss

--- codeDeploy/afterInstall.sh
+++ codeDeploy/afterInstall.sh
@@ -3,8 +3,11 @@ sudo unzip build_artifact.zip
 sudo unzip deploySrcCodeBuild.zip
 aws configure set default.region us-east-1
 aws configure list
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch/config.json -s
-cd ..
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/home/ubuntu/webapp/logs/config.json \
+    -scd ..
 cd /home/ubuntu/webapp 
 sudo rm -rf node_modules
 sudo npm i
