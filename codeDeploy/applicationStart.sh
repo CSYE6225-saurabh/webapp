4a4299acecb6f0dd6f1b970a539eee8623162efc
Saurabh <ambardekar.s@northeastern.edu>
2021-11-14 16:37:01 -0500

    logs

--- codeDeploy/applicationStart.sh
+++ codeDeploy/applicationStart.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu/webapp
-sudo rm logs/logs/webapp.log
-sudo touch logs/webapp.log
-sudo chmod 666 logs/webapp.log
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a start
 sudo npm install
 sudo npm i pm2 -g
