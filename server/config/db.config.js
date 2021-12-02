9ecd79951b6b168b45d4bb96c8e512eed08a75cc
Saurabh <ambardekar.s@northeastern.edu>
2021-12-02 11:32:31 -0500

    demo12

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -4,11 +4,11 @@ const data = fs.readFileSync("/home/ubuntu/server/config.json");
 const temp = JSON.parse(data);
 module.exports = {
     HOST: temp.host.split(":")[0],
-    HOSTREADREPLICA: "replica.cprdck0ambbc.us-east-1.rds.amazonaws.com",
+    HOSTREADREPLICA: temp.hostReadReplics.split(":")[0],
     USER: temp.username,
     PASSWORD: temp.password,
     DB: temp.database,
-    dialect : temp.hostReadReplics.split(":")[0],
+    dialect : "mysql",
     s3: temp.s3,
     port: temp.port,
     topicArn : temp.topic_arn
